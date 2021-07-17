50273011ebfbcf52c808f9eace8ce31a799ccc65
ajaysagarn <ajaysagarn@gmail.com>
2021-07-17 09:43:04 -0500

    follow naming convention

--- SQL/initializeDB.sql
+++ SQL/initializeDB.sql
@@ -13,10 +13,10 @@ Create TABLE tb_user(
 );
 
 Create TABLE phone_number(
-    userId INT,
+    user_id INT,
     phoneNo INT(12),
-    PRIMARY KEY (userId, phoneNo),
-    FOREIGN KEY (userId) REFERENCES tb_user(id)
+    PRIMARY KEY (user_id, phoneNo),
+    FOREIGN KEY (user_id) REFERENCES tb_user(id)
 );
 
 Create Table game(
@@ -30,63 +30,63 @@ Create Table game(
 
 CREATE TABLE price_record(
     id INT AUTO_INCREMENT,
-    gameId INT,
+    game_id INT,
     timestamp DATETIME NOT NULL,
-    currentPrice BIGINT NOT NULL,
+    current_price BIGINT NOT NULL,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (gameId) REFERENCES game(id)
+    FOREIGN KEY (game_id) REFERENCES game(id)
 );
 
 
 CREATE TABLE genre(
     id INT AUTO_INCREMENT,
-    genreName VARCHAR(215) NOT NULL,
-    parentGenre INT,
+    genre_name VARCHAR(215) NOT NULL,
+    parent_genre INT,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (parentGenre) REFERENCES Genre (id)
+    FOREIGN KEY (parent_genre) REFERENCES Genre (id)
 );
 
 CREATE TABLE publisher(
     id INT AUTO_INCREMENT,
-    publisherName VARCHAR(215) NOT NULL,
+    publisher_name VARCHAR(215) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE game_genre(
-    gameId INT,
-    genreId INT,
-    PRIMARY KEY (gameId, genreId),
-    FOREIGN KEY (gameId) REFERENCES game(id),
-    FOREIGN KEY (genreId) REFERENCES genre(id)
+    game_id INT,
+    genre_id INT,
+    PRIMARY KEY (game_id, genre_id),
+    FOREIGN KEY (game_id) REFERENCES game(id),
+    FOREIGN KEY (genre_id) REFERENCES genre(id)
 );
 
 CREATE TABLE user_interested_game(
-    gameId INT,
-    userId INT,
-    PRIMARY KEY (gameId, userId),
-    FOREIGN KEY (gameId) REFERENCES game(id),
-    FOREIGN KEY (userId) REFERENCES tb_user(id)
+    game_id INT,
+    user_id INT,
+    PRIMARY KEY (game_id, user_id),
+    FOREIGN KEY (game_id) REFERENCES game(id),
+    FOREIGN KEY (user_id) REFERENCES tb_user(id)
 );
 
 CREATE TABLE game_publisher(
-    gameId INT,
-    publisherId INT,
-    PRIMARY KEY (gameId, publisherId),
-    FOREIGN KEY (gameId) REFERENCES game(id),
-    FOREIGN KEY (publisherId) REFERENCES publisher(id)
+    game_id INT,
+    publisher_id INT,
+    PRIMARY KEY (game_id, publisher_id),
+    FOREIGN KEY (game_id) REFERENCES game(id),
+    FOREIGN KEY (publisher_id) REFERENCES publisher(id)
 );
 
 CREATE TABLE alert_log(
     id INT AUTO_INCREMENT,
-    userId INT,
-    gameId INT,
+    user_id INT,
+    game_id INT,
     timestamp DATETIME NOT NULL,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (userId) REFERENCES tb_user(id),
-    FOREIGN KEY (gameId) REFERENCES game(id)
+    FOREIGN KEY (user_id) REFERENCES tb_user(id),
+    FOREIGN KEY (game_id) REFERENCES game(id)
 )
 
 
